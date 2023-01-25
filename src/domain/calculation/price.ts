b4d5d99d3e6eecf5c50b09c3aaf41f7d87f64ac2
Robinson Soares <soares.robinson@gmail.com>
2023-01-25 12:07:35 -0300

    refactor: implement calculation interface on all calculation price

--- src/domain/calculation/price.ts
+++ src/domain/calculation/price.ts
@@ -1,32 +1,62 @@
-export class PriceCalculation {
-    calculate(gameMode: string, distance: number, type: string): number {
+import { GameMode } from "../../shared/game-mode";
+import { SeatCategory } from "../../shared/seat-category";
+import { CalculationInterface } from "../interfaces/calculation-interface";
 
-        if (distance <= 0) {
-            throw ('Distance must be positive')
+export type PriceProps = {
+    gameMode: GameMode,
+    distance: number,
+    type: SeatCategory
+}
+
+export class PriceCalculation implements CalculationInterface<PriceProps> {
+    calculate(value: PriceProps): number {
+        const { gameMode, distance, type } = value
+
+        let result: number
+
+        if (!gameMode) {
+            throw new Error('GameMode is required')
         }
 
-        if (gameMode === 'easy' && type === 'economic') {
-            return ((0.4006 * distance) + 170)
+        if (!type) {
+            throw new Error('Seat Type is required')
         }
 
-        if (gameMode === 'easy' && type === 'business') {
-            return ((0.8006 * distance) + 560)
+        if (distance < 0 || distance === null || distance === undefined) {
+            throw new Error('Distance must be positive')
         }
 
-        if (gameMode === 'easy' && type === 'first') {
-            return ((1.2006 * distance) + 1200)
+        if (gameMode === GameMode.EASY) {
+            result = this.calculateEasyMode(distance, type)
+        } else {
+            result = this.calculateRealismMode(distance, type)
         }
 
-        if (gameMode === 'realismo' && type === 'economic') {
-            return ((0.3006 * distance) + 150)
+        return result
+
+    }
+
+    private calculateEasyMode(distance: number, type: SeatCategory): number {
+        if (type === SeatCategory.ECONOMY) {
+            return (0.4006 * distance + 170)
         }
 
-        if (gameMode === 'realismo' && type === 'business') {
-            return ((0.6006 * distance) + 500)
+        if (type === SeatCategory.FIRST_CLASS) {
+            return (1.2006 * distance + 1200)
         }
 
-        if (gameMode === 'realismo' && type === 'first') {
-            return ((0.9006 * distance) + 1000)
+        return (0.8006 * distance + 560)
+    }
+
+    private calculateRealismMode(distance: number, type: SeatCategory): number {
+        if (type === SeatCategory.ECONOMY) {
+            return (0.3006 * distance + 150)
         }
+
+        if (type === SeatCategory.FIRST_CLASS) {
+            return (0.9006 * distance + 1000)
+        }
+
+        return (0.6006 * distance + 500)
     }
 }
